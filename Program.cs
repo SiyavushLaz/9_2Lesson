7b1b082623752e5edbd2e2a5a9c7424dbdf4512f
Siyavush-L <s.lazgiyev@gmail.com>
2023-04-20 15:42:27 +0600

    Gotov

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение M: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+        for (int i = m; i <= n; i++)
+        {
+            sum += i;
+        }
+
+        Console.WriteLine("Сумма натуральных чисел в промежутке от {0} до {1}: {2}", m, n, sum);
+    }
+}
